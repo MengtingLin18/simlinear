28f7c6ae2cecea938307480bd3ae6787bf006e02
Mengting Lin <mengting.lin@colostate.edu>
2021-02-04 21:43:26 -0700

    help
    help change

--- R/Rfunction.R
+++ R/Rfunction.R
@@ -1,7 +1,17 @@
 #' Fit a simple linear regression model
 #' @param  x vector, the predictor variable in simple linear regression
 #' @param  y vector, the response variable in simple linear regression
-#' @return a list of regression result
+#' @return a list of regression result.
+#' coefficient: the estimated regression coefficents, intercept and slope.
+#' standard_error: the estimated standard errors for the coefficients,intercept and slope.
+#' conf_interval: the 95% confidence intervals for population intercept and slope.
+#' residuals: the residuals, that is response minus fitted values.
+#' predicted: the predicted value for each observation.
+#' @examples
+#' slm <- simp_lin_R(x = cars$speed, y = cars$dist)
+#' slm$standard_error
+#' slm$conf_interval
+#' slm$residuals
 #' @export
 simp_lin_R <- function(x, y){
   if(!class(x) %in% c('numeric', 'integer')) stop('x must be a numeric vector.')
